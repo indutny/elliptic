c469ce04f6c78cd0071444a0b0c8471ae441b056
Fedor Indutny <fedor@indutny.com>
2014-04-23 23:49:12 +0400

    curve: fix getting Infinity

---
+++ test/ecdsa-test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var elliptic = require('../');
+
+describe('ECDSA', function() {
+  it('should work with secp256k1 NIST curve', function() {
+    var curve = elliptic.nist.secp256k1;
+    assert(curve);
+
+    var ecdsa = new elliptic.ecdsa(curve);
+    var keys = ecdsa.genKeyPair();
+    var msg = new elliptic.bn('deadbeef', 16);
+
+    // Sign and verify
+    var signature = ecdsa.sign(msg, keys.priv);
+    assert(ecdsa.verify(msg, keys.pub));
+
+    // Wrong public key
+    var keys = ecdsa.genKeyPair();
+    assert(!ecdsa.verify(msg, keys.pub));
+  });
+});
