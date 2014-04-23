536645746c18986ad37c4f80712009eeb15af584
Fedor Indutny <fedor@indutny.com>
2014-04-23 14:03:54 +0400

    ec: point addition

--- test/curve-test.js
+++ test/curve-test.js
@@ -14,6 +14,10 @@ describe('Curve', function() {
       '18',
       '16');
     assert(p.validate());
+    assert(p.dbl().validate());
+    assert(p.dbl().add(p).validate());
+    assert(p.dbl().add(p.dbl(p)).validate());
+    assert(p.dbl().add(p.dbl(p)).eq(p.add(p).add(p).add(p).add(p)));
   });
 
   it('should work with secp112k1', function() {
@@ -28,6 +32,7 @@ describe('Curve', function() {
       '0948 7239995a 5ee76b55 f9c2f098',
       'a89c e5af8724 c0a23e0e 0ff77500');
     assert(p.validate());
+    assert(p.dbl().validate());
   });
 
   it('should work with secp256k1', function() {
@@ -44,5 +49,6 @@ describe('Curve', function() {
       '483ada77 26a3c465 5da4fbfc 0e1108a8 fd17b448 a6855419 9c47d08f fb10d4b8'
     );
     assert(p.validate());
+    assert(p.dbl().validate());
   });
 });
