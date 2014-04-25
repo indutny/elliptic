1d016d04159051325903a7fccb51fffa69d7d275
Fedor Indutny <fedor@indutny.com>
2014-04-25 17:25:51 +0400

    ecdsa: more security...

--- test/ecdsa-test.js
+++ test/ecdsa-test.js
@@ -18,4 +18,28 @@ describe('ECDSA', function() {
     var keys = ecdsa.genKeyPair();
     assert(!ecdsa.verify(msg, signature, keys.pub));
   });
+
+  describe('RFC6979 vector', function() {
+    function test(opt) {
+      it('should not fail on "' + opt.name + '"', function() {
+        var ecdsa = elliptic.ecdsa({
+          curve: opt.curve,
+          hash: opt.hash
+        });
+
+        var dgst = opt.hash().update(opt.message).digest();
+        console.log(ecdsa.sign(dgst, opt.key));
+      });
+    }
+
+    test({
+      name: 'ECDSA, 192 Bits (Prime Field)',
+      curve: elliptic.nist.p192,
+      hash: elliptic.hash.sha256,
+      message: 'sample',
+      key: '6fab034934e4c0fc9ae67f5b5659a9d7d1fefd187ee09fd4',
+      r: '4b0b8ce98a92866a2820e20aa6b75b56382e0f9bfd5ecb55',
+      s: 'ccdb006926ea9565cbadc840829d8c384e06de1f1e381b85'
+    });
+  });
 });
