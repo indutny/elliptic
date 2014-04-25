fac69e4581c2ed03bfc93614e186e43d108535c0
Fedor Indutny <fedor@indutny.com>
2014-04-25 14:38:13 +0400

    lib: add hmac and fix hash update

--- test/hash-test.js
+++ test/hash-test.js
@@ -3,17 +3,36 @@ var elliptic = require('../');
 
 describe('Hash', function() {
   it('should support sha256', function() {
-    function hash(msg) {
-      return elliptic.hash.sha256().update(msg).digest('hex');
+    function hash(msg, enc) {
+      return elliptic.hash.sha256().update(msg, enc).digest('hex');
     }
+
+    assert.equal(elliptic.hash.sha256.blockSize, 512);
+    assert.equal(elliptic.hash.sha256.outSize, 256);
 
     var test = [
       [ 'abc',
         'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad' ],
       [ 'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq',
         '248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1' ],
+      [ 'deadbeef',
+        '5f78c33274e43fa9de5659265c1d917e25c03722dcb0b8d27db8d5feaa813953',
+        'hex' ],
     ];
-    for (var i = 0; i < test.length; i++)
-      assert.equal(hash(test[i][0]), test[i][1]);
+    for (var i = 0; i < test.length; i++) {
+      var msg = test[i][0];
+      var res = test[i][1];
+      var enc = test[i][2];
+
+      var hash = elliptic.hash.sha256().update(msg, enc).digest('hex');
+      assert.equal(hash, res);
+
+      // Split message
+      var hash = elliptic.hash.sha256()
+                         .update(msg.slice(0, 2), enc)
+                         .update(msg.slice(2), enc)
+                         .digest('hex');
+      assert.equal(hash, res);
+    }
   });
 });
---
+++ test/hmac-test.js
@@ -0,0 +1,47 @@
+var assert = require('assert');
+var elliptic = require('../');
+var utils = elliptic.utils;
+
+describe('Hmac', function() {
+  it('should support hmac-sha256', function() {
+    function hmac(opt) {
+      return elliptic.hmac(elliptic.hash.sha256, opt.key, 'hex')
+                     .update(opt.msg)
+                     .digest('hex');
+    }
+
+    var test = [
+      {
+        key: '00010203 04050607 08090A0B 0C0D0E0F' +
+             '10111213 14151617 18191A1B 1C1D1E1F 20212223 24252627' +
+             '28292A2B 2C2D2E2F 30313233 34353637 38393A3B 3C3D3E3F',
+        msg: 'Sample message for keylen=blocklen',
+        res: '8bb9a1db9806f20df7f77b82138c7914d174d59e13dc4d0169c9057b133e1d62'
+      },
+      {
+        key: '00010203 04050607' +
+             '08090A0B 0C0D0E0F 10111213 14151617 18191A1B 1C1D1E1F',
+        msg: 'Sample message for keylen<blocklen',
+        res: 'a28cf43130ee696a98f14a37678b56bcfcbdd9e5cf69717fecf5480f0ebdf790'
+      },
+      {
+        key: '00010203' +
+             '04050607 08090A0B 0C0D0E0F 10111213 14151617 18191A1B' +
+             '1C1D1E1F 20212223 24252627 28292A2B 2C2D2E2F 30313233' +
+             '34353637 38393A3B 3C3D3E3F 40414243 44454647 48494A4B' +
+             '4C4D4E4F 50515253 54555657 58595A5B 5C5D5E5F 60616263',
+        msg: 'Sample message for keylen=blocklen',
+        res: 'bdccb6c72ddeadb500ae768386cb38cc41c63dbb0878ddb9c7a38a431b78378d'
+      },
+      {
+        key: '00' +
+             '01020304 05060708 090A0B0C 0D0E0F10 11121314 15161718' +
+             '191A1B1C 1D1E1F20 21222324 25262728 292A2B2C 2D2E2F30',
+        msg: 'Sample message for keylen<blocklen, with truncated tag',
+        res: '27a8b157839efeac98df070b331d593618ddb985d403c0c786d23b5d132e57c7'
+      }
+    ];
+    for (var i = 0; i < test.length; i++)
+      assert.equal(hmac(test[i]), test[i].res);
+  });
+});
