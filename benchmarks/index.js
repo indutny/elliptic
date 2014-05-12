569e347d62c7cdcdf8aa46e758d29ed0b31186cb
Fedor Indutny <fedor@indutny.com>
2014-05-13 00:59:38 +0400

    benchmarks: add benchmarks

---
+++ benchmarks/index.js
@@ -0,0 +1,68 @@
+var assert = require('assert');
+var benchmark = require('benchmark');
+var hash = require('hash.js');
+var elliptic = require('../');
+var eccjs = require('eccjs');
+
+var benchmarks = [];
+
+function add(op, a, b, c) {
+  benchmarks.push({
+    name: op,
+    start: function start() {
+      var suite = new benchmark.Suite;
+
+      console.log('Benchmarking: ' + op);
+      suite
+        .add('elliptic#' + op, a)
+        .add('eccjs#' + op, b)
+        .on('cycle', function(event) {
+          console.log(String(event.target));
+        })
+        .on('complete', function() {
+          console.log('------------------------');
+          console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+        })
+        .run();
+      console.log('========================');
+    }
+  });
+}
+
+function start() {
+  var re = process.argv[2] ? new RegExp(process.argv[2], 'i') : /./;
+
+  benchmarks.filter(function(b) {
+    return re.test(b.name);
+  }).forEach(function(b) {
+    b.start();
+  });
+}
+
+var str = 'big benchmark against elliptic';
+
+var m1 = hash.sha256().update(str).digest();
+var c1 = elliptic.ecdsa(elliptic.nist.secp256k1);
+var k1 = c1.genKeyPair();
+var s1 = c1.sign(m1, k1);
+assert(c1.verify(m1, s1, k1));
+
+var m2 = eccjs.sjcl.hash.sha256.hash('big benchmark against elliptic');
+var c2 = eccjs.sjcl.ecc.curves.k256;
+var k2 = eccjs.sjcl.ecc.ecdsa.generateKeys(c2, 0);
+var s2 = k2.sec.sign(m2, 0);
+assert(k2.pub.verify(m2, s2));
+
+add('sign', function() {
+  c1.sign(m1, k1);
+}, function() {
+  k2.sec.sign(m2, 0);
+});
+
+add('verify', function() {
+  c1.verify(m1, s1, k1);
+}, function() {
+  k2.pub.verify(m2, s2);
+});
+
+start();
